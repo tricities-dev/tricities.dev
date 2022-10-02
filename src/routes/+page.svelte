793d32cfae9029b5054e4b46f3a343fff230a0fd
fudgemasterultra <79956447+fudgemasterultra@users.noreply.github.com>
2022-10-02 18:27:17 -0400

    Installed SASS
    Installed SASS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>TriDev SvelteKit Base Init w/ Typescript!!</h1>
+<style lang="scss">
+    h1{
+       color: red; 
+    }
+</style>
+
+<h1 class="heading">TriDev SvelteKit Base Init w/ Typescript!!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
