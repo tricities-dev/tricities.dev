c99625c0d7e94c5d4bc35d202f4fb92b4cac24c9
Jeff Rainey <rainey.jeff@gmail.com>
2022-10-04 22:26:45 -0400

    adding some link spacing work and adding the discord link

--- src/components/link.svelte
+++ src/components/link.svelte
@@ -1,25 +1,41 @@
 <script lang="ts">
     export let link: string;
     export let text: string;
+    export let cssClass: string = '';
 </script>
 
-<style>
+<style lang="scss">
+    // todo move to variables.scss
+    $white: #fff;
+    $black: #000;
+
     .border {
         display: flex;
         justify-content: center;
         padding: 1rem;
-        border: 1px solid white;
+        border: 1px solid $white;
     }
-
     a {
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        color: white;
+        color: $white;
+        background: transparent;
         text-decoration: none;
+        transition: all .3s ease-in-out;
+        &.block {
+            display: block;
+            width: 100%;
+        }
+
+        &.text-center {
+            text-align: center;
+        }
+        &:hover {
+            background: $white;
+            color: $black;
+        }
     }
 </style>
 
-<a href="{link}">
-    <div class="border">
-        {text}
-    </div>
+<a href="{link}" class="border {cssClass}">
+    {text}
 </a>
