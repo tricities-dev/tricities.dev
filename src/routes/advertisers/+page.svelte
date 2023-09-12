9f5226e75beb04765bdf83f85992c149bce12791
msmith <msmith@cenergy.com>
2023-09-12 14:51:03 -0400

    added in qr code packages

--- src/routes/advertisers/+page.svelte
+++ src/routes/advertisers/+page.svelte
@@ -1,17 +1,73 @@
 <script lang="ts">
   import type { Type } from "../../app.d";
-
-  const supporters: Type.Advertiser[] = [];
+  import { QRCodeImage } from "svelte-qrcode-image";
+  const advertiser: Type.Advertiser = {
+    link: "http://google.com",
+    logo: "/bitsum-logo-light.webp",
+    title: "Bitsum",
+    description: "Describe what it it is",
+  };
 </script>
 
 <div class="container">
   <div class="full-screen-ad">
-    <div class="sides advertiser-logo" />
-    <div class="sides advertiser-info" />
+    <div class="sides advertiser-logo">
+      <img class="center image-size" src={advertiser.logo} />
+    </div>
+    <div class="sides advertiser-info">
+      <p class="title-text">{advertiser.title}</p>
+
+      <div class="content-info">
+        <p>{advertiser.link}</p>
+        <p>{advertiser.description}</p>
+      </div>
+      <div>
+        <div class="qr-code">
+          <QRCodeImage text={advertiser.link} />
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
 <style>
+  .qr-code {
+    display: flex;
+    justify-content: center;
+  }
+
+  .content-info {
+    margin-left: 20%;
+    margin-right: 20%;
+    color: aliceblue;
+  }
+
+  p {
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 1rem;
+    font-weight: 600;
+  }
+
+  .title-text {
+    font-size: 5rem;
+    color: white;
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  .center {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 25%;
+    transform: translate(-50%, -50%);
+  }
+
+  .image-size {
+    width: 25%;
+    height: auto;
+  }
+
   .container {
     position: fixed;
     inset: 0;
